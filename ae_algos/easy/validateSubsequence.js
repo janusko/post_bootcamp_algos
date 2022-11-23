9007abb3e316b6ad15bcb18e80b6a13fda1faebb
Ryan Janusko <rp.janusko@gmail.com>
2022-11-23 14:53:53 -0500

    explaination typo

--- ae_algos/easy/validateSubsequence.js
+++ ae_algos/easy/validateSubsequence.js
@@ -1,6 +1,6 @@
 // Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one.
 
-// A subsequence of an array is a set of numbers that aren't necessaryily adjacent in the arrat, but that are in teh same order as they appear in teh array. For instance, the numbers [1, 3, 4] form a subsuqence of the array [1, 2, 3, 4], and so do the numbers [2,4]. Note that a single number in an array and the array itself are both valid subseqences of the array.
+// A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array, but that are in the same order as they appear in the array. For instance, the numbers [1, 3, 4] form a subsuqence of the array [1, 2, 3, 4], and so do the numbers [2,4]. Note that a single number in an array and the array itself are both valid subseqences of the array.
 
 function isValidSubsequence(array, sequence) {
     if (array.length == 0) {
